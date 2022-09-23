3ab4e3d53feb2e546976a1c844c14ed00e32410f
Zachary Vance <za3k@za3k.com>
2022-09-23 18:00:14 -0400

    Done

---
+++ archive/timelog-analysis/make.sh
@@ -0,0 +1,11 @@
+timelog-graph "drive|drove" >drive.txt
+timelog-graph "floss" >floss.txt
+timelog-graph "food" >food.txt
+timelog-graph "read|bk|book" > "read.txt"
+timelog-graph "sleep|slept" > "sleep.txt"
+timelog-graph "teeth" > teeth.txt
+timelog-graph "tv" >tv.txt
+timelog-graph "wake|woke" >wake.txt
+timelog-graph "walk" >walk.txt
+timelog-graph "youtube|yt" >youtube.txt
+
