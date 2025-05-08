2d160d01c1cc271ae23f2d07e2bde1be51082e94
Zachary Vance <za3k@za3k.com>
2025-05-08 17:07:34 -0400

    MicroRPG reference

---
+++ archive/microrpg/generate.py
@@ -0,0 +1,33 @@
+import pystache
+import subprocess
+
+in_file = "MicroRPGs.csv"
+out_file = "microrpgs.md"
+template = "template.md"
+max_players = 9
+
+def csv2md(csv_text):
+    return subprocess.check_output(["csv2md"], input=csv_text)
+
+def run_csv_query(query):
+    return subprocess.check_output(["q", "-H", "-d,", query])
+
+if __name__ == '__main__':
+    with open("template.md") as f:
+        template = f.read()
+    
+    table = []
+
+    for n in range(1, max_players+1):
+        query = "select Game,Players,Time,Genre,Tone,Format,Players,Complexity from MicroRPGs.csv WHERE PlayersAll like \"%{}%\"".format(n)
+
+        filtered_csv = run_csv_query(query)
+        table.append({
+            "markdown": csv2md(filtered_csv),
+            "players": n,
+        })
+
+    out = pystache.render(template, {"table": table})
+
+    with open("microrpgs.md", "w") as f:
+        f.write(out)
---
+++ archive/microrpg/template.md
@@ -0,0 +1,21 @@
+[za3k](/) &gt; [games](/games) &gt; micro-rpg book
+
+<style>
+@media print {
+    .page-break:not(:last-child) { display: block; visibility: hidden; page-break-after: always; }
+    .dont-print { display: none; }
+}
+</style>
+
+<div class="dont-print">Press Ctrl-P to print on most web browsers.</div>
+
+{{#table}}
+
+## Ultimate Micro-RPG Book: Games for {{players}} players
+
+{{markdown}}
+
+<hr class="page-break">
+
+{{/table}}
+
